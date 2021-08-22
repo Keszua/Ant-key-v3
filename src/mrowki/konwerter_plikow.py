7e64f4c99850d1868e0747bfe441ed3d455a2f38
Karlik <keszua@gmail.com>
2021-08-22 21:19:08 +0200

    Podszykowany konwerter_plikow

---
+++ src/mrowki/konwerter_plikow.py
@@ -0,0 +1,19 @@
+# Skrypt, który otwiera i przeszukuje plik .svg 
+# Plik svg musi zawieerać odpowiednie nazwy warts i podwarst
+# Szukane wartsy:
+# Należy przygotować plik np: "7R00_Camponotus.svn"
+# Dodać go do tablicy nazw plików
+# Wyniki zapsuje do "7R00_Camponotus.js"
+
+
+import time
+
+listaPlikow = ['7R00_Camponotus.svg' ]
+
+
+
+
+
+
+
+time_start = time.time()
