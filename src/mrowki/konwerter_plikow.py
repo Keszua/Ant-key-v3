35a91be78001d2e5c858e08bf77a1e4d0bc2793c
Karol_Michalczyk <k.michalczyk@humansoft.pl>
2021-08-23 16:01:08 +0200

    Prace nad tworzneim pliku przez skrypt pythona

--- src/mrowki/konwerter_plikow.py
+++ src/mrowki/konwerter_plikow.py
@@ -7,13 +7,98 @@
 
 
 import time
-
-listaPlikow = ['7R00_Camponotus.svg' ]
-
-
-
-
-
-
+import os
 
 time_start = time.time()
+
+
+listaPlikow = ['7R00_Camponotus' ]
+
+fileName=(f"{listaPlikow[0]}.js")
+
+def file_path(relative_path):
+    dir = os.path.dirname(os.path.abspath(__file__))
+    split_path = relative_path.split("/")
+    new_path = os.path.join(dir, *split_path)
+    return new_path
+
+# with open(file_path(fileName), "w") as f:
+#     f.write("Powerful you have become.")
+
+# mrowka = {
+    
+#     cialo: {
+#         gl: {x: 0,  y: 0},
+#         n1: {x: 0,  y: 0},
+#         n2: {x: 0,  y: 0},
+#         n3: {x: 0,  y: 0},
+#         st: {x: 0,  y: 0},
+#         d: [ ],
+#     },
+
+#     noga1: {
+#         odn1: {
+#             c1: {x: 0, y: 0 },
+#             c2: {x: 0, y: 0 },
+#             path: [
+#                 {   style: {  zIndex: 6, visibility: "visible" , stroke: '#592e00', strokeWidth: 1,  strokeOpacity: 1, strokeMiterlimit:4, fill: '#834e40', fillOpacity: 1 },
+#                     d: '', },
+#             ],
+#         },
+#         odn2: {
+#             c1: {x: 0, y: 0 },
+#             c2: {x: 0, y: 0 },
+#             path: [
+#                 {   style: {  zIndex: 6, visibility: "visible" , stroke: '#592e00', strokeWidth: 1,  strokeOpacity: 1, strokeMiterlimit:4, fill: '#834e40', fillOpacity: 1 },
+#                     d: '', },
+#             ],
+#         },
+#         odn3: {
+#             c1: {x: 0, y: 0 },
+#             c2: {x: 0, y: 0 },
+#             path: [
+#                 {   style: {  zIndex: 6, visibility: "visible" , stroke: '#592e00', strokeWidth: 1,  strokeOpacity: 1, strokeMiterlimit:4, fill: '#834e40', fillOpacity: 1 },
+#                     d: '', },
+#             ],
+#         },
+#         odn4: {
+#             c1: {x: 0, y: 0 },
+#             c2: {x: 0, y: 0 },
+#             path: [
+#                 {   style: {  zIndex: 6, visibility: "visible" , stroke: '#592e00', strokeWidth: 1,  strokeOpacity: 1, strokeMiterlimit:4, fill: '#834e40', fillOpacity: 1 },
+#                     d: '', },
+#             ],
+#         },
+
+#     }
+# }
+
+mrowka = {
+    'cialo': {
+        "g1": {'x': 0,  'y': 0},
+        }, 
+    5: "e",
+}
+
+fileW = open(file_path(fileName), mode="w") # w : po otwarciu, kasowana jest zawartość
+fileW.write("export const mrowka = \n")
+
+# fileW.write("    cialo: {\n\n")
+# fileW.write("    },\n\n")
+
+for key in mrowka.keys():
+    print(key)
+
+    fileW.write(f'    {key}:')
+
+
+# fileW.write("\n}\n")
+fileW.close()
+
+
+
+
+time_stop = time.time()
+print("operacja trwała: ", time_stop - time_start)
+
+
