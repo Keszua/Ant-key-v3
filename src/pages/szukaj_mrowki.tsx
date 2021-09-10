afb153d118c0b8bec2486964fa563ae2525b525c
Karol_Michalczyk <k.michalczyk@humansoft.pl>
2021-09-10 12:23:37 +0200

    Inicjalizacja scss
    Nie udana proba ogarnięcia react-slick

--- src/pages/szukaj_mrowki.tsx
+++ src/pages/szukaj_mrowki.tsx
@@ -1,10 +1,19 @@
 import React  from 'react';
-import './szukaj_mrowki.css';
+import ReactDOM from 'react-dom';
+// import './szukaj_mrowki.css';
 //import "~slick-carousel/slick/slick.css"; 
 //import "~slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 //import Slider from 'node_modules\react-slick\dist\react-slick.js'
 // Slider ze strony: https://react-slick.neostack.com/docs/api
+/*
+instalowałem:
+
+npm install react-slick --save 
+npm install slick-carousel --save  
+npm i --save-dev @types/react-slick
+*/
+
 
 
 const SzukajMrowkiPage = () => {
@@ -15,36 +24,34 @@ const SzukajMrowkiPage = () => {
         return (
           <div
             className={className}
-            style={{ ...style, display: "block", background: "red" }}
+            // style={{ ...style, display: "block", background: "red" }}
+            style={{ ...style, background: "red" }}
             onClick={onClick}
           />
         );
       }
       
     const SamplePrevArrow = (props :any) => {
-    const { className, style, onClick } = props;
-    return (
-        <div
-        className={className}
-        style={{ ...style, display: "block", background: "green" }}
-        onClick={onClick}
-        />
-    );
+        const { className, style, onClick } = props;
+        console.log('style', style);
+        return (
+            <div
+                className={className}
+                // style={{ ...style, display: "block", background: "green" }}
+                style={{ ...style, background: "green" }}
+                
+                onClick={onClick}
+            />
+        );
     }
 
 
     const SliderH_settings = {
         // className: "center",
-        // centerPadding: "20px",
+        centerPadding: "20px",
+        // width: '100px',
         className: "slider variable-width",
-
-        dots: true,
-        infinite: true,
-        // speed: 500,
-        // slidesToShow: 3,
-        // slidesToScroll: 1,
-        // initialSlide: 0,
-        // centerMode: true,
+        // variableWidth: true,
         nextArrow: <SampleNextArrow />,
         prevArrow: <SamplePrevArrow />
 
@@ -58,91 +65,93 @@ const SzukajMrowkiPage = () => {
 
     return ( 
         <>
-        <div>
-            Okno glowne
-            <svg className="inline" viewBox="0 0 600 300" xmlns="http://www.w3.org/2000/svg"
-                style={{ width:'600px', height:'300px', background:'gray' }}
-            >
+            <div >
+                <div className='informacje'>
+                    Okno glowne <br/>
+                </div>
+                <svg className="inline" viewBox="0 0 600 300" xmlns="http://www.w3.org/2000/svg"
+                    style={{ width:'600px', height:'300px', background:'gray', margin: '0 5% 0 5%' }}
+                >
 
-            </svg>
-        </div>
-        
-        <div>
-
-            <Slider {...SliderH_settings}
-                dots= {false}
-                infinite={true}
-                speed= {500}
-                slidesToShow= {3}
-                slidesToScroll= {1}
-                initialSlide= {0}
-                centerMode= {true}
-                beforeChange = {function(currentSlide, nextSlide) {
-                    console.log("before change", currentSlide, nextSlide);
-                  }}
-                afterChange= { function(currentSlide) {
-                    console.log("after change", currentSlide);
-                }}
-            >
-                <div>
-                    <h3>0</h3>
-                </div>
-                <div>
-                    <h3>1</h3>
-                </div>
-                <div>
-                    <h3>2</h3>
-                </div>
-                <div>
-                    <h3>3</h3>
-                </div>
-                <div>
-                    <h3>4</h3>
-                </div>
-                <div>
-                    <h3>5</h3>
-                </div>
-                <div>
-                    <h3>6</h3>
-                </div>
-            </Slider>
-
-            <Slider 
-                vertical= {true}
-                verticalSwiping= {true}
-                infinite= {true}
-                slidesToShow= {3}
-                slidesToScroll= {1}
-                initialSlide= {0}
-                speed= {500}
-                // centerMode= {true}
-                beforeChange = {function(currentSlide, nextSlide) {
-                    console.log("before change", currentSlide, nextSlide);
-                  }}
-                afterChange= { function(currentSlide) {
-                    console.log("after change", currentSlide);
-                }}
+                </svg>
+            </div>
+            {/* <div className="ramka_na_dwa_slidery"> */}
             
-            >
-                <div style= {{display: "flex", justifyContent: 'flex-start' }}>
-                    <div> <h3>A</h3> </div>  
-                    <div> <h3>A</h3> </div>  
-                </div>
-                <div>
-                    <h3>1</h3>
-                </div>
-                <div>
-                    <h3>2</h3>
-                </div>
-                <div>
-                    <h3>3</h3>
-                </div>
-            </Slider>
+                {/* <div className='ramka_na_slider ramka_na_slider__poziomy' > */}
 
+                    <Slider 
+                    //  {...SliderH_settings}
+                        infinite={true}
+                        speed= {500}
+                        slidesToShow= {3}
+                        slidesToScroll= {1}
+                        initialSlide= {0}
+                        centerMode= {true}
+                        // centerPadding = {"20px"}
+                        // variableWidth = {true}
+                        dots= {false}
+                        // beforeChange = {function(currentSlide, nextSlide) {
+                        //     console.log("before change", currentSlide, nextSlide);
+                        // }}
+                        // afterChange= { function(currentSlide) {
+                        //     console.log("after change", currentSlide);
+                        // }}
+                    >
+                            <div className={'sl_okienko'}>
+                                djjkjsdd0
+                                jakiś tekst
+                                <p>fdfljfjf</p>
+                            </div>
+                            <div className={'sl_okienko'}>
+                                <h3>1</h3>jhfdjjdhf
+                            </div>
+                            <div className={'sl_okienko'}>
+                                <h3>2</h3>
+                            </div>
+                            <div className={'sl_okienko'}>
+                                <h3>3</h3>
+                            </div>
+                        <div className={'sl_okienko'}>
+                            <h3>4</h3>
+                        </div>
+                    </Slider>
+                {/* </div> */}
 
+                {/* <div className='ramka_na_slider ramka_na_slider__pionowy' >
+                    <Slider className='slider slider__pionowy'
+                        vertical= {true}
+                        verticalSwiping= {true}
+                        infinite= {true}
+                        slidesToShow= {2}
+                        slidesToScroll= {1}
+                        initialSlide= {0}
+                        speed= {500}
+                        // centerMode= {true}
+                        beforeChange = {function(currentSlide, nextSlide) {
+                            console.log("before change", currentSlide, nextSlide);
+                        }}
+                        afterChange= { function(currentSlide) {
+                            console.log("after change", currentSlide);
+                        }}
+                    
+                    >
+                        <div className={'sl_okienko'}>
+                            <div > <h3>A</h3> </div>  
+                            <div> <h3>A</h3> </div>  
+                        </div>
+                        <div className={'sl_okienko'}>
+                            <h3>1</h3>
+                        </div>
+                        <div className={'sl_okienko'}>
+                            <h3>2</h3>
+                        </div>
+                        <div className={'sl_okienko'}>
+                            <h3>3</h3>
+                        </div>
+                    </Slider>
+                </div> */}
 
-        </div>
-            
+            {/* </div>     */}
         </>
     )
 
